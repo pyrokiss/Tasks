ff3a9c48a52f8e1a0f00dd1dad27bf4aaa3d1781
pyro.chernomor@gmail.com <pyro.chernomor@gmail.com>
2020-11-26 15:12:29 +0200

    Добавил функцию для вывода матрицы

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,22 +2,19 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-	const unsigned int n = 2; 
-	int matrix[n][n] = { {3, 5}, {4, 6} };
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << setw(n) << matrix[i][j];
+void printMatrix(int *matrix, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << setw(size) << matrix[i * size + j];
 		}
 		cout << endl;
 	}
-
-	if (n == 2) {
-		int d = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
-		cout << d << endl;
-	}
-
-	return 0;
 }
 
+int main()
+{
+	const unsigned int n = 3;
+	int matrix[n][n] = { {3, 5, 5}, {4, 6, 1}, {1, 1, 1} };
+	printMatrix(&matrix[0][0], n);
+	return 0;
+}
